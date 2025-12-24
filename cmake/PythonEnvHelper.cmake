c9cf10ad244efc7a8f3868245147491a72c8e4a7
tmontaigu <thomas.montaigu@laposte.net>
2025-12-24 12:34:51 +0100

    Support pybind11 3.0

--- cmake/PythonEnvHelper.cmake
+++ cmake/PythonEnvHelper.cmake
@@ -1,6 +1,64 @@
 macro(ensure_pybind11_cmake_module_is_in_path)
+  # Get pybind11 major version
   execute_process(
-    COMMAND "${PYTHON_EXECUTABLE}" "-m" "pybind11" "--cmake"
+    COMMAND "${PYTHON_EXECUTABLE}" -m pybind11 --version
+    RESULT_VARIABLE _pyb_res
+    OUTPUT_VARIABLE _pyb_out
+    ERROR_VARIABLE _pyb_err
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    ERROR_STRIP_TRAILING_WHITESPACE
+  )
+
+  if(NOT _pyb_res EQUAL 0)
+    message(FATAL_ERROR
+      "Failed to run '${PYTHON_EXECUTABLE} -m pybind11 --version' (exit ${_pyb_res}). "
+      "Stderr: ${_pyb_err}"
+    )
+  endif()
+
+  set(_ver_str "${_pyb_out}")
+  # Try to capture major.minor.patch or major.minor or just major
+  set(_parsed FALSE)
+  string(REGEX MATCH "([0-9]+)\\.([0-9]+)\\.([0-9]+)" _m3 "${_ver_str}")
+  if(NOT "${_m3}" STREQUAL "")
+    set(_major "${CMAKE_MATCH_1}")
+    set(_minor "${CMAKE_MATCH_2}")
+    set(_patch "${CMAKE_MATCH_3}")
+    set(_parsed TRUE)
+  endif()
+
+  if(NOT _parsed)
+    string(REGEX MATCH "([0-9]+)\\.([0-9]+)" _m2 "${_ver_str}")
+    if(NOT "${_m2}" STREQUAL "")
+      set(_major "${CMAKE_MATCH_1}")
+      set(_minor "${CMAKE_MATCH_2}")
+      set(_patch "0")
+      set(_parsed TRUE)
+    endif()
+  endif()
+
+  if(NOT _parsed)
+    string(REGEX MATCH "([0-9]+)" _m1 "${_ver_str}")
+    if(NOT "${_m1}" STREQUAL "")
+      set(_major "${CMAKE_MATCH_1}")
+      set(_minor "0")
+      set(_patch "0")
+      set(_parsed TRUE)
+    endif()
+  endif()
+
+  if(NOT _parsed)
+    message(FATAL_ERROR "Could not parse pybind11 version from: '${_ver_str}'")
+  endif()
+
+  if (_major STREQUAL "3")
+    set(DIR_ARG "--cmakedir")
+  else()
+    set(DIR_ARG "--cmake")
+  endif()
+
+  execute_process(
+    COMMAND "${PYTHON_EXECUTABLE}" "-m" "pybind11" "${DIR_ARG}"
     RESULT_VARIABLE _PYTHON_SUCCESS
     OUTPUT_VARIABLE PYBIND11_CMAKE_MODULES_PATH
   )
